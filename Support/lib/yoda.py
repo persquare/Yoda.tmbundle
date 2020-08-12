662daba0a806c12bbc37bc889a49fee1fef06a49
Per Persson <persquare@me.com>
2020-08-12 14:32:14 +0200

    Use PythonHelpers bundle

--- Support/lib/yoda.py
+++ Support/lib/yoda.py
@@ -4,71 +4,74 @@
 # <https://github.com/lawrenceakka/python-jedi.tmbundle>
 import os
 import sys
-import glob
-import subprocess
-import plistlib
 import functools
 
 import jedi
 
-env = os.environ
-envvars = ['TM_BUNDLE_SUPPORT']
-sys.path[:0] = [env[v]+u'/lib' for v in envvars if env[v] not in sys.path]
+
+# Jedi API overview: https://jedi.readthedocs.io/en/latest/docs/api.html
+
+# The following jedi functionality is used:
+# -----------------------------------------
+#   script.completions()
+#   script.call_signatures()
+#   script.usages()
+#   script.goto_definitions()
+#   script.goto_assignments()
+
+# Consider using the following:
+# -----------------------------
+# Name.get_signatures() -> List[Signature]. Signatures are similar to CallSignature.
+#   Name.params is therefore deprecated.
+# Signature.to_string() to format signatures.
+# Signature.params -> List[ParamName], ParamName has the following additional attributes infer_default(),
+#   infer_annotation(), to_string(), and kind.
+# Name.execute() -> List[Name], makes it possible to infer return values of functions.
+# Script(...).completions(fuzzy=True)
+# Added Project support. This allows a user to specify which folders Jedi should work with.
+# Added support for Refactoring. The following refactorings have been implemented:
+#   Script.rename, Script.inline, Script.extract_variable and Script.extract_function.
+# Added Script.get_syntax_errors to display syntax errors in the current script.
+# Added code search capabilities both for individual files and projects.
+#   The new functions are Project.search, Project.complete_search, Script.search and Script.complete_search.
+# Added Script.help to make it easier to display a help window to people.
+#   Now returns pydoc information as well for Python keywords/operators.
+
+# Notes
+# -----
+# Big Script API Changes:
+#   The line and column parameters of jedi.Script are now deprecated
+#   completions deprecated, use complete instead
+#   goto_assignments deprecated, use goto instead
+#   goto_definitions deprecated, use infer instead
+#   call_signatures deprecated, use get_signatures instead
+#   usages deprecated, use get_references instead
+#   jedi.names deprecated, use jedi.Script(...).get_names()
 
 
-# from plistlib import writePlistToString, readPlistFromString
-def writePlistToString(value):
-    return plistlib.dumps(value)
-
-def readPlistFromString(data):
-    return plistlib.loads(data)
-    
-jedi.settings.case_insensitive_completion = False
-jedi.settings.add_bracket_after_function = True
-
-dialog = env['DIALOG']
-
-def _call_dialog(command, *args):
-    """ Call the Textmate Dialog process
-
-    command is the command to invoke.
-    args are the strings to pass as arguments
-    a dict representing the plist returned from DIALOG is returned
-
+try:
+    envvars = ['TM_PYTHON_HELPERS_BUNDLE_SUPPORT', 'TM_BUNDLE_SUPPORT']
+    sys.path[:0] = [os.path.join(os.environ[v], 'lib') for v in envvars]
+except:
+    errmsg = """
+    The PythonHelpers bundle required, see<br/>
+    <a href=https://github.com/persquare/PythonHelpers.tmbundle>
+    github.com/persquare/PythonHelpers.tmbundle
+    </a>
     """
-    popen_args = [dialog, command]
-    popen_args.extend(args)
-    result = subprocess.check_output(popen_args)
-    return result
+    sys.stderr.write(errmsg)
+    sys.exit(205)
 
-def register_images(imgdir):
-    imglist = glob.glob(imgdir+'/*.png')
-    imgnames = [os.path.basename(img).rsplit('.', 1)[0] for img in imglist]
-    for (name, img) in zip(imgnames, imglist):
-        _call_dialog('images', '--register', writePlistToString({name:img}))
-    return imgnames
+# import TextMate as tm
+from TextMate import dialog
 
-def present_popup(suggestions, typed='', extra_word_chars='_', return_choice=False):
-    retval = _call_dialog('popup',
-                          '--suggestions', writePlistToString(suggestions),
-                          '--alreadyTyped', typed,
-                          '--additionalWordCharacters', '_',
-                          '--returnChoice' if return_choice else '')
-    return readPlistFromString(retval) if retval else {}
-
-def present_menu(menu_items):
-    selections = [{'title':item} for item in menu_items]
-    retval = _call_dialog('menu', '--items', writePlistToString(selections))
-    return readPlistFromString(retval) if retval else {}
-
-def present_tooltip(text, is_html=False):
-    _call_dialog('tooltip', '--html' if is_html else '--text', text)
+env = os.environ
 
 
 def open_in_editor(path, line=1, column=0):
     # os.system("open txmt://open/?url=file:///%s&line=%d&column=%d" % (path, int(line), int(column)))
     os.system("mate -l%d:%d %s" % (int(line), int(column), path))
-     
+
 def _active_selection():
     return 'TM_SELECTED_TEXT' in env
 
@@ -79,7 +82,7 @@ def _get_line_column():
         line, col = lc.split(':')
     else:
         line = env['TM_LINE_NUMBER']
-        col = env['TM_COLUMN_NUMBER']       
+        col = env['TM_COLUMN_NUMBER']
     return (int(line), int(col)-1)
 
 
@@ -93,7 +96,7 @@ def get_script(debug=None):
         path = env['TM_FILE_PATH'] if not source else None
         script = jedi.Script(source, line, col, path)
     except Exception as e: # AttributeError, KeyError:
-        present_tooltip("{}".format(e))
+        dialog.present_tooltip("{}".format(e))
         pass
     return script
 
@@ -106,10 +109,10 @@ def completion():
         sys.stdout.write(completions[0].complete)
         return
     # Prepare data for popup
-    icons = register_images(env['TM_BUNDLE_SUPPORT'] + '/icons')
+    icons = dialog.register_images(env['TM_BUNDLE_SUPPORT'] + '/icons')
     typed = env.get('TM_CURRENT_WORD', '').lstrip('.')
     suggestions = [{'display':c.name, 'image':c.type if c.type in icons else 'none'} for c in completions]
-    return present_popup(suggestions, typed)
+    dialog.present_popup(suggestions, typed)
 
 
 def _prep_arg(arg, i):
@@ -145,8 +148,9 @@ def signature():
     signatures = []
     # Really don't know when we could get > 1 result here...
     # Discard for now, and use headings for grouping if I find out it happens
-    if len(completions)>1: present_tooltip("Mutiple options\n"+str(completions))
-    
+    if len(completions)>1:
+        dialog.present_tooltip("Mutiple options\n"+str(completions))
+
     c = completions[0]
     descriptions = [p.description.rstrip(', ') for p in c.params]
 
@@ -155,7 +159,7 @@ def signature():
     descriptions = descriptions[c.index:]
 
     args, kargs, vargs = _prepare_arg_snippets(descriptions)
-    
+
     # Present (up to) three options:
     # 1) only positional arguments
     # 2) positional and named arguments
@@ -163,7 +167,7 @@ def signature():
     minimal = args
     sensible = args + kargs
     maximal = args + kargs + vargs
-    
+
     minimal_key = ', '.join(descriptions[:len(minimal)])
     sensible_key = ', '.join(descriptions[:len(sensible)])
     maximal_key = ', '.join(descriptions[:len(maximal)])
@@ -172,17 +176,18 @@ def signature():
     if minimal_key: options[minimal_key] = ', '.join(minimal)
     if sensible_key: options[sensible_key] = ', '.join(sensible)
     if maximal_key: options[maximal_key] = ', '.join(maximal)
-    
+
     if not options:
         return
-    
+
     if len(options) == 1:
         sys.stdout.write(options.values()[0])
         sys.exit(204)
-    
-    selection = present_menu(options.keys())
-    if selection:
-        key = selection['title']
+
+    selection = tuple(options.keys())
+    choice = dialog.present_menu(selection)
+    if choice is not None:
+        key = selection[choice]
         sys.stdout.write(options[key])
         sys.exit(204)
 
@@ -190,13 +195,13 @@ def signature():
 def _goto_description(description):
     # description is instance of Definition()
     if description.in_builtin_module():
-        present_tooltip("builtin: %s" % description.description)
+        dialog.present_tooltip("builtin: %s" % description.description)
         return
     file = description.module_path or env['TM_FILEPATH'] if 'TM_FILEPATH' in env else None
     if not file:
-        present_tooltip("Unknown location")
+        dialog.present_tooltip("Unknown location")
         return
-    # Actually goto selected location here...    
+    # Actually goto selected location here...
     # present_tooltip("Jumping to %s:%s" % (file, description.line))
     open_in_editor(file, description.line, description.column)
 
@@ -206,9 +211,9 @@ def _cmp(a, b):
 def _usage_cmp(a, b):
     order = _cmp(a.module_name, b.module_name)
     if order == 0:
-        order = _cmp(a.line, b.line)    
+        order = _cmp(a.line, b.line)
     return order
-        
+
 def _process_usages(us):
     # Sort per file:line
     us.sort(key=functools.cmp_to_key(_usage_cmp))
@@ -220,7 +225,7 @@ def _process_usages(us):
         key = "[%s:%s] %s" % (u.module_name or file, u.line, u.description)
         options[key] = u
         menu_items.append(key)
-    # Return menu_items to preserve ordering   
+    # Return menu_items to preserve ordering
     return (menu_items, options)
 
 
@@ -229,11 +234,11 @@ def usage():
     usages = script.usages()
     if not usages:
         return
-        
-    menu_items, options = _process_usages(usages)    
-    selection = present_menu(menu_items)
-    if selection:
-        key = selection['title']
+
+    menu_items, options = _process_usages(usages)
+    choice = dialog.present_menu(menu_items)
+    if choice is not None:
+        key = menu_items[choice]
         _goto_description(options[key])
 
 def definitions():
@@ -244,10 +249,10 @@ def definitions():
     if len(definitions) == 1:
         _goto_description(definitions[0])
         return
-    menu_items, options = _process_usages(definitions)    
-    selection = present_menu(menu_items)
-    if selection:
-        key = selection['title']
+    menu_items, options = _process_usages(definitions)
+    choice = dialog.present_menu(menu_items)
+    if choice is not None:
+        key = menu_items[choice]
         _goto_description(options[key])
 
 def assignments():
@@ -258,12 +263,12 @@ def assignments():
     if len(assignments) == 1:
         _goto_description(assignments[0])
         return
-    menu_items, options = _process_usages(assignments)    
-    selection = present_menu(menu_items)
-    if selection:
-        key = selection['title']
+    menu_items, options = _process_usages(assignments)
+    choice = dialog.present_menu(menu_items)
+    if choice is not None:
+        key = menu_items[choice]
         _goto_description(options[key])
-    
+
 def quickdoc():
     """Present tooltip with short docs"""
     script = get_script()
@@ -272,10 +277,9 @@ def quickdoc():
     try:
         definitions = script.goto_definitions()
     except jedi.NotFoundError:
-        present_tooltip('No definition found')
+        dialog.present_tooltip('No definition found')
         return
-        
+
     if definitions:
         definition = definitions[0]
-        present_tooltip(definition.docstring())
-        
+        dialog.present_tooltip(definition.docstring())
